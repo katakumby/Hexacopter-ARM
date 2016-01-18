31155f6898245afe2e21caf407f8750813250a37
katakumby <katakumby1996@gmail.com>
2016-01-18 01:57:32 +0100

    Fix

---
+++ Main/Reciver/Reciver.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Reciver.ino
+ Created:	1/18/2016 1:56:35 AM
+ Author:	katak
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
